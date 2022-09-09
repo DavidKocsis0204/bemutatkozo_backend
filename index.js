34b73d65329345fe9560e0ec73e9660064b8d0d9
DavidKocsis0204 <kocsisdavid20020204@gmail.com>
2022-09-09 10:34:03 +0200

    nem

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const port=3000;
+const path=require('path');
+const { status } = require('express/lib/response');
+app.use(express.urlencoded({extended:true}));
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.status(200).sendFile(path.join(__dirname+'/index.html'));
+
+})
+app.get('/hobbi', function (req, res) {
+    res.status(200).sendFile(path.join(__dirname+'/hobbies.html'));
+})
+app.get('/contacts', function (req, res) {
+    res.status(200).sendFile(path.join(__dirname+'/contacts.html'));
   
 })
+app.post('/message', function(req, res){
+    console.log(req.body);
+    res.status(200).send("Az üzenetet kézbesítettük!");
+})
+
+app.use('/css', express.static(path.join(__dirname + '/css')));
+app.use('/img', express.static(path.join(__dirname+'/img')));
+
+app.listen(port);
+console.log(`app listening on port ${port}`)
